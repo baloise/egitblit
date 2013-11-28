136d1577146d85a7b31a664860df95c40a1fff6b
Michael Bagatzky <michael.bagatzky@baloise.ch>
2013-11-28 17:06:12 +0100

    Rebuild

--- GitBlitRepositoryExplorerPlugin/src/ch/basler/plugin/gitblitexplorer/pref/Initializer.java
+++ GitBlitRepositoryExplorerPlugin/src/ch/basler/plugin/gitblitexplorer/pref/Initializer.java
@@ -17,7 +17,6 @@ public class Initializer extends AbstractPreferenceInitializer {
 	@Override
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-	    store.setDefault(GitBlitExplorerPrefPage.KEY_GITBLIT_URL, "https://vcs.balgroupit.com/gitblit");
 	    store.setDefault(GitBlitExplorerPrefPage.KEY_GITBLIT_USER, System.getProperty("user.name"));
 	    store.setDefault(GitBlitExplorerPrefPage.KEY_GITBLIT_DCLICK, GitBlitExplorerPrefPage.VALUE_GITBLIT_DCLICK_GITBLIT);
 	}
