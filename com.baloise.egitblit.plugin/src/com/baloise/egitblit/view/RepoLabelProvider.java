6a7ccae78293245b77a186e9a9eca010746f9b83
MicBag <michael.bagatzky@baloise.ch>
2013-12-19 11:26:59 +0100

    Cleanup LabelProvider: Removing dead code

--- com.baloise.egitblit.plugin/src/com/baloise/egitblit/view/RepoLabelProvider.java
+++ com.baloise.egitblit.plugin/src/com/baloise/egitblit/view/RepoLabelProvider.java
@@ -1,75 +1,23 @@
 package com.baloise.egitblit.view;
 
 import org.eclipse.jface.viewers.CellLabelProvider;
-import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
 
-import com.baloise.egitblit.view.model.GitBlitViewModel;
 import com.baloise.egitblit.view.model.GroupViewModel;
 import com.baloise.egitblit.view.model.ProjectViewModel;
 
 /**
  * @see CellLabelProvider
  * @author MicBag
- * 
- */
-public class RepoLabelProvider extends CellLabelProvider implements ILabelProvider, ITableLabelProvider{
+ * extends CellLabelProvider  
+ **/
 
-	@Override
-	public void update(ViewerCell cell){
-		Object element = cell.getElement();
-		int columnIndex = cell.getColumnIndex();
-		if(element == null){
-			return;
-		}
-		cell.setText(getColumnText(element,columnIndex));
-	}
+public class RepoLabelProvider implements ITableLabelProvider{
 
-	@Override
-	public String getToolTipText(Object element){
-		String str = null;
-		if(element instanceof GitBlitViewModel){
-			str = ((GitBlitViewModel)element).getToolTip();
-		}
-		return str;
-	}
-
-	@Override
-	public Point getToolTipShift(Object object){
-		return new Point(5, 5);
-	}
-
-	@Override
-	public int getToolTipDisplayDelayTime(Object object){
-		return 500;
-	}
-	
-	@Override
-	public int getToolTipTimeDisplayed(Object object) {
-		return 5000;
-	}
-
-	
-	@Override
-	public Image getImage(Object element){
-		return null;
-	}
-
-	@Override
-	public String getText(Object element){
-		if(element instanceof GitBlitViewModel){
-			return ((GitBlitViewModel)element).getName();
-		}
-		return null;
-	}
-
-	
 	@Override
 	public Image getColumnImage(Object element, int columnIndex){
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -97,4 +45,23 @@ public class RepoLabelProvider extends CellLabelProvider implements ILabelProvid
 		}
 		return null;
 	}
+
+	@Override
+	public void addListener(ILabelProviderListener listener){
+	}
+
+	@Override
+	public void dispose(){
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property){
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener){
+	}
+	
+	
 }
